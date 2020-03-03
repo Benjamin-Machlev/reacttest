607d3137486ade819dad3af677d76896e4ce024d
Benjamin-Machlev <beni251@gmail.com>
2020-03-03 21:57:41 +0200

    Updates

---
+++ precache-manifest.d49450f3b935e81b3a4b2b59dc3b2156.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea68147c91ea0f597605882ff3af7fe1",
+    "url": "/reacttest/index.html"
+  },
+  {
+    "revision": "4e385790d6646dde9a27",
+    "url": "/reacttest/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "71ac6c280eb281a0b602",
+    "url": "/reacttest/static/js/2.621574dd.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/reacttest/static/js/2.621574dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e385790d6646dde9a27",
+    "url": "/reacttest/static/js/main.0da2e71c.chunk.js"
+  },
+  {
+    "revision": "5b227ee8865d342b81d2",
+    "url": "/reacttest/static/js/runtime-main.215b032f.js"
+  }
+]);
